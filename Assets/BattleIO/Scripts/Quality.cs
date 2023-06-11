5655209ee7ed7482e7240e637f5843f58b5894cd
Maxime66410 <max66410@hotmail.fr>
2023-06-11 23:58:45 +0200

    Base File Unity

---
+++ Assets/BattleIO/Scripts/Quality.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Quality : MonoBehaviour
+{
+
+    public Canvas Graphic;
+    public Button Low;
+    public Button Medium;
+    public Button High;
+
+    void Start()
+    {
+        Graphic = Graphic.GetComponent<Canvas>();
+        Low = Low.GetComponent<Button>();
+        Medium = Medium.GetComponent<Button>();
+        High = High.GetComponent<Button>();
+        Graphic.enabled = false;
+
+    }
+    public void lowww()
+    {
+        QualitySettings.SetQualityLevel(0);
+    }
+    public void meddd()
+    {
+        QualitySettings.SetQualityLevel(1);
+    }
+    public void highhh()
+    {
+        QualitySettings.SetQualityLevel(3);
+    }
+
+}
